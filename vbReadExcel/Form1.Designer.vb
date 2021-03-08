bc3710d47f77dacbd471c8596340b30a0231557a
Muhamad Rizky <muhamad.profindo.com>
2021-03-08 15:59:39 +0700

    Add CheckBox Column

--- vbReadExcel/Form1.Designer.vb
+++ vbReadExcel/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.textBoxFileName_1 = New System.Windows.Forms.TextBox()
@@ -32,16 +32,21 @@ Partial Class Form1
         Me.buttonBrowseFile_2 = New System.Windows.Forms.Button()
         Me.textBoxFileName_2 = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DataGridView_2 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView_1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.buttonCompare = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.buttonExportToExcel = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        CType(Me.DataGridView_2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView_1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(10, 478)
+        Me.Label1.Location = New System.Drawing.Point(12, 249)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(54, 13)
         Me.Label1.TabIndex = 1
@@ -49,14 +54,14 @@ Partial Class Form1
         '
         'textBoxFileName_1
         '
-        Me.textBoxFileName_1.Location = New System.Drawing.Point(87, 478)
+        Me.textBoxFileName_1.Location = New System.Drawing.Point(89, 249)
         Me.textBoxFileName_1.Name = "textBoxFileName_1"
         Me.textBoxFileName_1.Size = New System.Drawing.Size(370, 20)
         Me.textBoxFileName_1.TabIndex = 2
         '
         'buttonBrowseFile_1
         '
-        Me.buttonBrowseFile_1.Location = New System.Drawing.Point(463, 478)
+        Me.buttonBrowseFile_1.Location = New System.Drawing.Point(465, 249)
         Me.buttonBrowseFile_1.Name = "buttonBrowseFile_1"
         Me.buttonBrowseFile_1.Size = New System.Drawing.Size(32, 20)
         Me.buttonBrowseFile_1.TabIndex = 3
@@ -66,7 +71,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(10, 522)
+        Me.Label2.Location = New System.Drawing.Point(12, 275)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(35, 13)
         Me.Label2.TabIndex = 4
@@ -75,7 +80,7 @@ Partial Class Form1
         'comboBoxSheet_1
         '
         Me.comboBoxSheet_1.FormattingEnabled = True
-        Me.comboBoxSheet_1.Location = New System.Drawing.Point(87, 522)
+        Me.comboBoxSheet_1.Location = New System.Drawing.Point(89, 275)
         Me.comboBoxSheet_1.Name = "comboBoxSheet_1"
         Me.comboBoxSheet_1.Size = New System.Drawing.Size(121, 21)
         Me.comboBoxSheet_1.TabIndex = 5
@@ -83,7 +88,7 @@ Partial Class Form1
         'comboBoxSheet_2
         '
         Me.comboBoxSheet_2.FormattingEnabled = True
-        Me.comboBoxSheet_2.Location = New System.Drawing.Point(571, 522)
+        Me.comboBoxSheet_2.Location = New System.Drawing.Point(89, 563)
         Me.comboBoxSheet_2.Name = "comboBoxSheet_2"
         Me.comboBoxSheet_2.Size = New System.Drawing.Size(121, 21)
         Me.comboBoxSheet_2.TabIndex = 11
@@ -91,7 +96,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(511, 522)
+        Me.Label3.Location = New System.Drawing.Point(12, 563)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(35, 13)
         Me.Label3.TabIndex = 10
@@ -99,7 +104,7 @@ Partial Class Form1
         '
         'buttonBrowseFile_2
         '
-        Me.buttonBrowseFile_2.Location = New System.Drawing.Point(964, 475)
+        Me.buttonBrowseFile_2.Location = New System.Drawing.Point(465, 537)
         Me.buttonBrowseFile_2.Name = "buttonBrowseFile_2"
         Me.buttonBrowseFile_2.Size = New System.Drawing.Size(32, 20)
         Me.buttonBrowseFile_2.TabIndex = 9
@@ -108,60 +113,117 @@ Partial Class Form1
         '
         'textBoxFileName_2
         '
-        Me.textBoxFileName_2.Location = New System.Drawing.Point(571, 475)
+        Me.textBoxFileName_2.Location = New System.Drawing.Point(89, 537)
         Me.textBoxFileName_2.Name = "textBoxFileName_2"
-        Me.textBoxFileName_2.Size = New System.Drawing.Size(387, 20)
+        Me.textBoxFileName_2.Size = New System.Drawing.Size(370, 20)
         Me.textBoxFileName_2.TabIndex = 8
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(511, 476)
+        Me.Label4.Location = New System.Drawing.Point(12, 538)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(54, 13)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "&File Name"
         '
-        'DataGridView2
+        'DataGridView_2
         '
-        Me.DataGridView2.AllowUserToOrderColumns = True
-        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView2.Location = New System.Drawing.Point(514, 12)
-        Me.DataGridView2.Name = "DataGridView2"
-        Me.DataGridView2.Size = New System.Drawing.Size(482, 450)
-        Me.DataGridView2.TabIndex = 6
+        Me.DataGridView_2.AllowUserToOrderColumns = True
+        Me.DataGridView_2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView_2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView_2.Location = New System.Drawing.Point(12, 302)
+        Me.DataGridView_2.Name = "DataGridView_2"
+        Me.DataGridView_2.Size = New System.Drawing.Size(1162, 233)
+        Me.DataGridView_2.TabIndex = 6
         '
-        'DataGridView1
+        'DataGridView_1
         '
-        Me.DataGridView1.AllowUserToOrderColumns = True
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(13, 13)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(482, 450)
-        Me.DataGridView1.TabIndex = 0
+        Me.DataGridView_1.AllowUserToOrderColumns = True
+        Me.DataGridView_1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView_1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView_1.Location = New System.Drawing.Point(13, 13)
+        Me.DataGridView_1.Name = "DataGridView_1"
+        Me.DataGridView_1.Size = New System.Drawing.Size(1161, 233)
+        Me.DataGridView_1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(465, 249)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(32, 20)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = ". . ."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'buttonCompare
+        '
+        Me.buttonCompare.Location = New System.Drawing.Point(293, 275)
+        Me.buttonCompare.Name = "buttonCompare"
+        Me.buttonCompare.Size = New System.Drawing.Size(204, 21)
+        Me.buttonCompare.TabIndex = 12
+        Me.buttonCompare.Text = "&Compare && Replace"
+        Me.buttonCompare.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(15, 602)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(286, 115)
+        Me.RichTextBox1.TabIndex = 13
+        Me.RichTextBox1.Text = "1. Import raw.xlsx pada table 1" & Global.Microsoft.VisualBasic.ChrW(10) & "2. Import template.xlsx pada table 2" & Global.Microsoft.VisualBasic.ChrW(10) & "3. Select da" &
+    "ta range di table raw" & Global.Microsoft.VisualBasic.ChrW(10) & "4. Klik Compare & Replace" & Global.Microsoft.VisualBasic.ChrW(10) & "5. Export data table ke excel"
+        '
+        'buttonExportToExcel
+        '
+        Me.buttonExportToExcel.Location = New System.Drawing.Point(216, 563)
+        Me.buttonExportToExcel.Name = "buttonExportToExcel"
+        Me.buttonExportToExcel.Size = New System.Drawing.Size(281, 21)
+        Me.buttonExportToExcel.TabIndex = 14
+        Me.buttonExportToExcel.Text = "&Export to Excel"
+        Me.buttonExportToExcel.UseVisualStyleBackColor = True
+        Me.buttonExportToExcel.UseWaitCursor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(216, 277)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(71, 17)
+        Me.CheckBox1.TabIndex = 15
+        Me.CheckBox1.Text = "Check All"
+        Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1008, 729)
+        Me.ClientSize = New System.Drawing.Size(1186, 729)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.buttonExportToExcel)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.buttonCompare)
         Me.Controls.Add(Me.comboBoxSheet_2)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.buttonBrowseFile_2)
         Me.Controls.Add(Me.textBoxFileName_2)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView_2)
         Me.Controls.Add(Me.comboBoxSheet_1)
         Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.buttonBrowseFile_1)
         Me.Controls.Add(Me.textBoxFileName_1)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.DataGridView_1)
         Me.KeyPreview = True
         Me.Name = "Form1"
         Me.Text = "Read Excel File (.xls, .xlsx)"
-        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView_2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView_1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -176,6 +238,11 @@ Partial Class Form1
     Friend WithEvents buttonBrowseFile_2 As Button
     Friend WithEvents textBoxFileName_2 As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents DataGridView2 As DataGridView
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents DataGridView_2 As DataGridView
+    Friend WithEvents DataGridView_1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents buttonCompare As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents buttonExportToExcel As Button
+    Friend WithEvents CheckBox1 As CheckBox
 End Class
--- vbReadExcel/Form1.vb
+++ vbReadExcel/Form1.vb
@@ -7,49 +7,40 @@ Public Class Form1
 
     End Sub
 
-    Private Sub buttonBrowseFile_1_Click(sender As Object, e As EventArgs) Handles buttonBrowseFile_1.Click
-        'Using openFileDialog As OpenFileDialog = New OpenFileDialog With {.Filter = "Excel Workbook|*.xlsx|Excel 97-2003 Workbook|*.xls"}
-        '    If openFileDialog.ShowDialog() = DialogResult.OK Then
-        '        textBoxFileName_1.Text = openFileDialog.FileName
-        '        Using streamFile = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read)
-        '            Using readerFile As IExcelDataReader = ExcelReaderFactory.CreateReader(streamFile)
-        '                Dim result As DataSet = readerFile.AsDataSet(New ExcelDataSetConfiguration() With {
-        '                                                             .ConfigureDataTable = Function(__) New ExcelDataTableConfiguration() With {
-        '                                                             .UseHeaderRow = True}})
-        '                tables = result.Tables
-        '                comboBoxSheet_1.Items.Clear()
-        '                For Each table As DataTable In tables
-        '                    comboBoxSheet_1.Items.Add(table.TableName)
-        '                Next
-        '            End Using
-        '        End Using
-        '    End If
-        'End Using
+    Private Sub buttonBrowseFile_1_Click(sender As Object, e As EventArgs) Handles buttonBrowseFile_1.Click, Button1.Click
         openFileExcelDialog(textBoxFileName_1, comboBoxSheet_1)
     End Sub
 
     Private Sub comboBoxSheet_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboBoxSheet_1.SelectedIndexChanged
         Dim dataTable = tables(comboBoxSheet_1.SelectedItem.ToString)
-        DataGridView1.DataSource = dataTable
+        Dim checkBoxDGV As New DataGridViewCheckBoxColumn
+        Dim checkBoxHeaderDGV As New DataGridViewColumnHeaderCell
+
+        DataGridView_1.DataSource = dataTable
+        checkBoxDGV.HeaderCell = checkBoxHeaderDGV
+        checkBoxDGV.Width = 40
+        checkBoxDGV.Name = "checkBoxDataGridView"
+        checkBoxDGV.HeaderText = ""
+        DataGridView_1.Columns.Insert(0, checkBoxDGV)
     End Sub
 
     Private Sub buttonBrowseFile_2_Click(sender As Object, e As EventArgs) Handles buttonBrowseFile_2.Click
         openFileExcelDialog(textBoxFileName_2, comboBoxSheet_2)
     End Sub
 
-    Public Sub openFileExcelDialog(textBox As TextBox, comboBox As ComboBox)
+    Public Sub openFileExcelDialog(textBoxOpenDialog As TextBox, comboBoxOpenDialog As ComboBox)
         Using openFileDialog As OpenFileDialog = New OpenFileDialog With {.Filter = "Excel Workbook|*.xlsx|Excel 97-2003 Workbook|*.xls"}
             If openFileDialog.ShowDialog() = DialogResult.OK Then
-                textBox.Text = openFileDialog.FileName
+                textBoxOpenDialog.Text = openFileDialog.FileName
                 Using streamFile = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read)
                     Using readerFile As IExcelDataReader = ExcelReaderFactory.CreateReader(streamFile)
                         Dim result As DataSet = readerFile.AsDataSet(New ExcelDataSetConfiguration() With {
                                                                      .ConfigureDataTable = Function(__) New ExcelDataTableConfiguration() With {
                                                                      .UseHeaderRow = True}})
                         tables = result.Tables
-                        comboBox.Items.Clear()
+                        comboBoxOpenDialog.Items.Clear()
                         For Each table As DataTable In tables
-                            comboBox.Items.Add(table.TableName)
+                            comboBoxOpenDialog.Items.Add(table.TableName)
                         Next
                     End Using
                 End Using
@@ -59,7 +50,7 @@ Public Class Form1
 
     Private Sub comboBoxSheet_2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles comboBoxSheet_2.SelectedIndexChanged
         Dim dataTable = tables(comboBoxSheet_2.SelectedItem.ToString)
-        DataGridView2.DataSource = dataTable
+        DataGridView_2.DataSource = dataTable
     End Sub
 
     Private Sub textBoxFileName_1_TextChanged(sender As Object, e As EventArgs) Handles textBoxFileName_1.TextChanged
@@ -71,6 +62,17 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub buttonGetText_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
 
     End Sub
 End Class
